cb5a8ba6389a8c13d6d2aba20e9db3875fe9cc31
Sven Schittecatte <sven.schittecatte@j4s.be>
2015-05-30 18:48:48 +0200

    working commit

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('watcher', [
   'watcher.controllers',
-  'watcher.services'
+  'watcher.services',
+  'ngAnimate'
 ]).config(['$compileProvider', function($compileProvider) {
             $compileProvider.imgSrcSanitizationWhitelist(/^\s*(http|https?|ftp|file|blob|content):|data:image\//);
         }]);
